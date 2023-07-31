09afc032da48f1dd0bf19ee46c54b6d881809892
David Curras <david.curras@radiumrocket.com>
2023-07-31 01:54:33 -0300

    Initial challenge assignment

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var pacman;
+var ghost;
+
+window.addEventListener('DOMContentLoaded', function(){
+    pacman = document.getElementById('pacman');
+    ghost = document.getElementById('ghost');
+    console.log(pacman);
+    console.log(ghost);
+});
