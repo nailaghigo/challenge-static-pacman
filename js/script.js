d35623902d776054b8a06861b5344a31e3d70dbd
Naila Ghigo <nailaghigo20@gmail.com>
2023-07-31 12:14:27 -0300

    Add movement to pacman

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 var pacman;
 var ghost;
+var pacmanX = 0, pacmanY = 0, step= 3;
+
 
 window.addEventListener('DOMContentLoaded', function(){
-    pacman = document.getElementById('pacman');
-    ghost = document.getElementById('ghost');
-    console.log(pacman);
-    console.log(ghost);
+  pacman = document.getElementById('pacman');
+  ghost = document.getElementById('ghost');
+
+  startGame(pacman)
 });
+
+function startGame(pacman) {
+  window.addEventListener('keydown', function(e) {
+    e.preventDefault();
+
+    const key = e.key;
+    switch (key) {
+      case "ArrowLeft":
+        pacmanX -= step
+        pacman.style.left = pacmanX + 'px'
+        break;
+      case "ArrowRight":
+        pacmanX += step
+        pacman.style.left = pacmanX + 'px'
+        break;
+      case "ArrowUp":
+        pacmanY -= step
+        pacman.style.top = pacmanY + 'px'
+        break;
+      case "ArrowDown":
+        pacmanY += step
+        pacman.style.top = pacmanY + 'px'
+        break;
+    }
+  })
+}
